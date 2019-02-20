b690377b2db0a3a3a66e1a0790d45046f865970c
ArshErgon <pgcomputer7@gmail.com>
2019-02-20 19:01:26 +0530

    'folio-heroku'

--- aboutme/models.py
+++ aboutme/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class AboutModel(models.Model):
     reason      =    models.CharField(max_length=50, default='ok')
-    image       =    models.ImageField(upload_to='upImg', null=True)
+    image       =    models.ImageField(upload_to='upImg', null=True, blank=True)
     message     =    models.TextField()
     email       =    models.EmailField(null=True)
 
